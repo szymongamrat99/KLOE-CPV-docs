8d425f83d2775c957aaf6d522e42bb76d552ef1c
Szymon Gamrat <gamrat@kitt.uj.edu.pl>
2025-02-23 13:01:00 +0100

    20250223_1300

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['clear1darray',['Clear1DArray',['../classKLOE_1_1pm00.html#a49be0d6e07ef2346d9ee705c4ae73577',1,'KLOE::pm00::Clear1DArray(UInt_t, Float_t *)'],['../classKLOE_1_1pm00.html#ac6a70aecf42b8557aac61b90ad4355b3',1,'KLOE::pm00::Clear1DArray(UInt_t, Int_t *)']]],
-  ['clear2darray',['Clear2DArray',['../classKLOE_1_1pm00.html#a947e551bbf225650989471b3cc60b7b4',1,'KLOE::pm00::Clear2DArray(UInt_t, UInt_t, Float_t **)'],['../classKLOE_1_1pm00.html#ad1fa2dc7bde1f414d4ddd47c8c1d8435',1,'KLOE::pm00::Clear2DArray(UInt_t, UInt_t, Int_t **)']]],
+  ['clear1darray',['Clear1DArray',['../classKLOE_1_1pm00.html#a08d7a3f9bacd91b8550a9fe564d219ec',1,'KLOE::pm00::Clear1DArray(UInt_t M, Float_t *array)'],['../classKLOE_1_1pm00.html#a8ad29bef1d8ca643b33235e6b2fdcb50',1,'KLOE::pm00::Clear1DArray(UInt_t M, Int_t *array)']]],
+  ['clear2darray',['Clear2DArray',['../classKLOE_1_1pm00.html#a1c9f00b942f0328ea0a9a253aff50242',1,'KLOE::pm00::Clear2DArray(UInt_t M, UInt_t N, Float_t **array)'],['../classKLOE_1_1pm00.html#ac1c77156672c8314bc036667b754a9aa',1,'KLOE::pm00::Clear2DArray(UInt_t M, UInt_t N, Int_t **array)']]],
   ['countrepeatingelements',['CountRepeatingElements',['../classKLOE_1_1pm00.html#abbd343d3b03f9a5713e5612a52956edc',1,'KLOE::pm00']]]
 ];
